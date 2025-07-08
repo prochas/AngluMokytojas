3c95e0ded6d3c727da012b7c745b8adedc94427f
tomas.prochorenko <tomas.prochorenko@okredo.lt>
2025-07-08 20:45:26 +0300

    new changes

---
+++ src/components/Header/Header.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+import Logo from "../../../public/images/logo.svg";
+
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className="fixed lg:px-28 px-5 top-0 left-0 w-full z-50 bg-white p-5 transition-shadow duration-300 shadow-none">
+      <div className="container mx-auto flex justify-between items-center">
+        <Image className="h-9 cursor-pointer" alt="Logo" src={Logo} />
+        <ul className="hidden lg:flex items-center gap-x-7 font-semibold">
+          <li className="group">
+            <Link
+              href="/"
+              className="text-gray-800 hover:text-blue-600 transition-colors duration-300"
+            >
+              Home
+            </Link>
+            <span className="w-0 transition-all duration-300 group-hover:w-full h-[2px] bg-black flex" />
+          </li>
+          <li className="group">
+            <Link
+              href="/about"
+              className="text-gray-800 hover:text-blue-600 transition-colors duration-300"
+            >
+              About
+            </Link>
+            <span className="w-0 transition-all duration-300 group-hover:w-full h-[2px] bg-black flex" />
+          </li>
+        </ul>
+        <Link
+          className="hidden relative lg:inline-block px-4 py-2 font-medium group"
+          href="/"
+        >
+          <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+          <span className="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
+          <span className="relative text-black group-hover:text-white flex items-center gap-x-3">
+            Resume
+          </span>
+        </Link>
+      </div>
+    </header>
+  );
+}
