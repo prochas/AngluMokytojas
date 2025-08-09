c155289a3303978ce240e2caea2f78ce632c1c7a
tomas.prochorenko <tomas.prochorenko@okredo.lt>
2025-08-09 14:05:58 +0300

    commit v31

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.anglumokytojas.lt', // Your live site URL
+  generateRobotsTxt: true, // Will also create robots.txt
+  sitemapSize: 7000,
+};
